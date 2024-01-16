046440ace033442b1ca10903ef0c51c52ac032fb
samiyonas <samuel.x.yonas@gmail.com>
2024-01-16 11:32:34 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    tmp = ''
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            tmp += chr(ord(i) - 32)
+        else:
+            tmp += i
+    print(tmp)
