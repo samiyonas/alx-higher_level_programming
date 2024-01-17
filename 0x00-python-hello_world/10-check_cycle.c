9c2709ad9e8d4985adc632e8be0368c02562079d
samiyonas <samuel.x.yonas@gmail.com>
2024-01-17 03:07:39 -0800

    cyclic linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_cycle - check for cycle in a singly linked list
+ * @list: singly linked list
+ * Return: int
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+	listint_t *temp = list;
+	listint_t *tmp = list->next;
+
+	while (temp != NULL)
+	{
+		while (tmp->next != NULL)
+		{
+			if (temp->next == tmp->next)
+			{
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+		temp = temp->next;
+	}
+	return (0);
+}
