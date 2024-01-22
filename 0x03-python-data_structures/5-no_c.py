2172c18e9b7ea9387c3fd8998f4618f1055dcdc2
samiyonas <samuel.x.yonas@gmail.com>
2024-01-22 08:58:58 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     string = ''
     for i in range(0, len(my_string)):
         if my_string[i] != 'c' or my_string[i] != 'C':
-            string += my_string[i]
+            string = string + my_string[i]
     return string
