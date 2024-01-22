6b830e4d0abdc6704cf261901964a92eeab639a8
samiyonas <samuel.x.yonas@gmail.com>
2024-01-22 14:49:27 -0800

    palindrome

---
+++ 0x03-python-data_structures/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
