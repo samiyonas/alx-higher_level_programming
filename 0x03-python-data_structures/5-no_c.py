6fc98050d3b4bfe7954ac413617f715137d4071e
samiyonas <samuel.x.yonas@gmail.com>
2024-01-22 08:50:35 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        string = ''
-        for i in my_string:
-            if i != 'c':
-                string += i
-        return string
+    string = ''
+    for i in my_string:
+        if i != 'c' or i != 'C':
+            string += i
+    return string
