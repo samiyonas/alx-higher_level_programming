94499a62ccd8d5a4f81dd871bcc0c7ce0588fcb3
samiyonas <samuel.x.yonas@gmail.com>
2024-01-24 08:09:30 -0800

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
     res = [[matrix[i][j] ** 2 for j in range(len(matrix[i]))]
            for i in range(len(matrix))]
     return res
