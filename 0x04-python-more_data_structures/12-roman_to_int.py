20772c648208d7e31a9ac8b73ec3a9aa4070444e
samiyonas <samuel.x.yonas@gmail.com>
2024-01-26 08:01:41 -0800

    roman number

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) not str:
+        return 0
     rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     i = 0
