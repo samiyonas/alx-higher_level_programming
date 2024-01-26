90311e6c9717bbb7cbe3e682b91a6a9a495e3da3
samiyonas <samuel.x.yonas@gmail.com>
2024-01-26 07:56:50 -0800

    roman string

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    i = 0
+    l = len(roman_string)
+    while i < l:
+        if i < l - 1 and rome_dict[roman_string[i]] < rome_dict[roman_string[i + 1]]:
+            res -= rome_dict[roman_string[i]]
+        else:
+            res += rome_dict[roman_string[i]]
+        i += 1
+    return res
