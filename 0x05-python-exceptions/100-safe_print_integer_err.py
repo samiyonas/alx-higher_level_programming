8e7a21886cfbec519271df418264c0d6ab764292
samiyonas <samuel.x.yonas@gmail.com>
2024-02-05 13:23:18 -0800

    safe integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ve:
-        sys.stderr.write("Exception: {}".format(ve))
+    except Exception as ve:
+        sys.stderr.write("Exception: {}\n".format(ve))
         return False
