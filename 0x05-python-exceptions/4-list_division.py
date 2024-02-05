c3ef5b7fea41e858c57ed91596ad6716c83141de
samiyonas <samuel.x.yonas@gmail.com>
2024-02-05 12:24:09 -0800

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     if len(my_list_1) == 0 and len(my_list_2) == 0:
         new.append(0)
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
