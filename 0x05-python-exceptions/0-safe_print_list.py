3a56bbe4a69bc914a098392c9be719f21a0576dc
samiyonas <samuel.x.yonas@gmail.com>
2024-02-05 09:32:55 -0800

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            if (my_list[i] != Exception):
+                print("{:d}".format(my_list[i]), end="")
+                counter += 1
+        except IndexError:
+            break
+        print()
+        return counter
