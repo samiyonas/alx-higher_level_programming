6f980918e16741bfa16de7ff22dfdbf3438f0c5a
samiyonas <samuel.x.yonas@gmail.com>
2024-02-10 02:04:18 -0800

    square but on coordinate plane

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+""" Define a Class """
+
+
+class Square:
+    """ A class that calculates area and uses property
+
+    Attribute:
+        __size (int) - private instance attribute(validated integer)
+        __position (tuple) - private instance attribute
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """ __init__ initializes objects
+
+        Attribute:
+            size (int) - will be validated to be assigned to __size
+            position (tuple) - tuple optionally initialized to (0, 0)
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ getter method
+
+        Return:
+            this method returns __size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter method """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """ position attribute getter method
+
+        Return:
+            return the tuple position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ position attribute setter method """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ calculates area using __size
+
+        Return:
+            the method returns the square of __size attribute
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints self.__size by self.__size of '#' """
+        if not self.__size == 0:
+            for i in range(0, self.__position[1]):
+                print()
+            for j in range(0, self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+        else:
+            print()
+
+    def __str__(self):
+        """ make the object printable """
+        string = ""
+        if not self.__size == 0:
+            for i in range(0, self.__position[1]):
+                string += "\n"
+            for j in range(0, self.__size):
+                string += " " * self.__position[1] + "#" * self.__size
+                if j != self.__size - 1:
+                    string += "\n"
+        else:
+            string += "\n"
+        return string
