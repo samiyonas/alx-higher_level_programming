1c7c054c6917c91195bd73e9e0dbe76a3dd80ce4
samiyonas <samuel.x.yonas@gmail.com>
2024-02-12 23:03:00 -0800

    minor fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,46 +1,42 @@
 #!/usr/bin/python3
 
-""" class with getter and setter property """
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """ Rectangle class
-    Attribute:
-        self.__width (int) - private instance width
-        self.__height (int) - private instance height
-    """
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
-        """ automatically initializes object
+        """Initialize a new Rectangle.
+
         Args:
-            width (int) - to be assigned to self.__width
-            height (int) - to be assigned to self.__height
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ getter method """
+        """Get/set the width of the rectangle."""
         return self.__width
-
-    @property
-    def height(self):
-        """ getter method """
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """ self.__width setter """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__height = value
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """ self.__height setter """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
