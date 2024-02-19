752420e815e1dd13f5e4feb6d1204733488e64ea
samiyonas <samuel.x.yonas@gmail.com>
2024-02-19 13:56:55 -0800

    new class inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python3
 
 """ define a class with area and integer validator """
-
-
-class BaseGeometry:
-    """ A cass with two methods """
-    def area(self):
-        """ raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ this functions validates an integer
-
-        Args:
-            name (str) - always presumed to be string
-            value (int) - number to be validated
-        Returns:
-            returns nothing but raises an error on error
-        """
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
