61acfd4a0dc8f2dd580fa75ceef4d5b59a8b1263
samiyonas <samuel.x.yonas@gmail.com>
2024-02-19 14:18:42 -0800

    full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" define a class with area and integer validator """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ A sub class with private instance attrs
+
+    Args:
+        self.__width (int) - width
+        self.__height (int) - height
+    Return:
+        returns nothing
+    """
+    def __init__(self, width, height):
+        """ magic method to initialize the object
+
+        Args:
+            width (int) - a width to be validated
+            height (int) - a height to be validated
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ area implementation """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}"/
+        "/{}".format(type(self).__name__, self.__width, self.__height)
