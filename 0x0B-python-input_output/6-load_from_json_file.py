6ee9e9680d5831fac4ba497b28409a1fd8cbd864
samiyonas <samuel.x.yonas@gmail.com>
2024-02-21 02:54:46 -0800

    load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" write a function to load object from JSON file """
+import json
+
+
+def load_from_json_file(filename):
+    """ loads JSON file to our python object """
+    with open(filename, 'r', encoding="utf-8") as myfile:
+        json.load(myfile)
