f6f91fb80d2c28cd9cb5fed066e804d41175e7e4
samiyonas <samuel.x.yonas@gmail.com>
2024-02-21 04:50:15 -0800

    student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" a class Student that defines a student """
+
+
+class Student:
+    """ class with one public instance method and initialization """
+    def __init__(self, first_name, last_name, age):
+        """ automatically initializes an object
+
+        Args:
+            first_name (str) - first name of student
+            last_name (str) - last name of student
+            age (int) - age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ returns obj __dict__ suitable for JSON serialization """
+        return obj.__dict__
