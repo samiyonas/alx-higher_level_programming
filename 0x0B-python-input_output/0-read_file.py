b72dd1794b3b43e4fae9a2d9bf5156806944b7eb
samiyonas <samuel.x.yonas@gmail.com>
2024-02-21 01:57:57 -0800

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """ read file and print it out """
     with open(filename, 'r', encoding="utf-8") as myfile:
         f = myfile.read()
-    print(f)
+        print(f)
