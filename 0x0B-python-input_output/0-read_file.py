c7060209264afe84ea8dd2c7b41fa570d9e8ff7f
samiyonas <samuel.x.yonas@gmail.com>
2024-02-21 01:53:40 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" a function that reads a text file """
+
+
+def read_file(filename=""):
+    """ read file and print it out """
+    with open(filename, 'r', encoding="utf-8") as myfile:
+        f = myfile.read()
+    print(f)
