7d0a9425c1e9f1a19f0175e722cff545d98b0ad3
samiyonas <samuel.x.yonas@gmail.com>
2024-02-24 00:29:02 -0800

    added area

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-""" 2-main """
+""" 3-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r1 = Rectangle(3, 2)
+    print(r1.area())
 
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r2 = Rectangle(2, 10)
+    print(r2.area())
 
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r3 = Rectangle(8, 7, 0, 0, 12)
+    print(r3.area())
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,7 @@ class Rectangle(Base):
         elif y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """ area of the rectangle """
+        return self.__width * self.__height
