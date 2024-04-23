47aa7a623e012bd2008e293f0a31a0a6d4de0f34
samiyonas <samuel.x.yonas@gmail.com>
2024-04-23 17:42:15 -0400

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const list = process.argv.slice(2, process.argv.length - 1);
-  let max = Math.max(...list);
+  const max = Math.max(...list);
   let Secondmax = list[0];
   for (let i = 0; i < list.length; i++) {
     if (list[i] > Secondmax && list[i] > max) {
