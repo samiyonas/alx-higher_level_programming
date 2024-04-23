5866cdd92017286c3051514292950467b82722a1
samiyonas <samuel.x.yonas@gmail.com>
2024-04-23 17:52:28 -0400

    second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,13 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.slice(2, process.argv.length - 1);
-  const max = Math.max(...list);
-  let Secondmax = list[0];
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] > Secondmax && list[i] > max) {
-      Secondmax = list[i];
+  const list = process.argv.slice(2, process.argv.length);
+  const arr = list.map(item => parseInt(item));
+  const max = Math.max(...arr);
+  let Secondmax = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > Secondmax && arr[i] < max) {
+      Secondmax = arr[i];
     }
   }
   console.log(Secondmax);
