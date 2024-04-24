cc46fd9dde95a7129d219a38763f1590f88cfcc6
samiyonas <samuel.x.yonas@gmail.com>
2024-04-24 07:24:15 -0400

    call me moby

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
   console.log(0);
-} else if (process.argv.length === 4) {
-  if (parseInt(process.argv[2]) > parseInt(process.argv[3])) {
-    console.log(parseInt(process.argv[3]));
-  } else {
-    console.log(parseInt(process.argv[2]));
-  }
 } else {
-  const list = process.argv.slice(2, process.argv.length);
-  const arr = list.map(item => parseInt(item));
-  const max = Math.max(...arr);
-  let Secondmax = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > Secondmax && arr[i] < max) {
-      Secondmax = arr[i];
-    }
-  }
-  console.log(Secondmax);
-}
+  const list = process.argv.slice(2, process.argv.length - 1);
+  let max = 
