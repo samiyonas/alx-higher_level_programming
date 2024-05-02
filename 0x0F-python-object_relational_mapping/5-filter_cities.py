1f4d364a87d1ca2886a346ab0a2c4e87e1b119f9
samiyonas <samuel.x.yonas@gmail.com>
2024-05-02 22:49:04 +0300

    all cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" all cities by states """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3],
+            host="localhost",
+            port=3306)
+    cur = db.cursor()
+    cur.execute(
+            "SELECT cities.name FROM cities "
+            "INNER JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s", (sys.argv[4],)
+            )
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        if i != len(rows) - 1:
+            print(rows[i][0], end=", ")
+        else:
+            print(rows[i][0])
+    cur.close()
+    db.close()
