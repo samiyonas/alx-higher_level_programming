ddcfdfe80d3ca4bcd0e54961da452228a05f87e0
samiyonas <samuel.x.yonas@gmail.com>
2024-05-02 17:49:03 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             port=3306)
     cur = db.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE states.name REGEXP '^N' "
+            "SELECT * FROM states WHERE states.name REGEXP '^N' OR '^n' "
             "ORDER BY states.id ASC"
             )
     rows = cur.fetchall()
