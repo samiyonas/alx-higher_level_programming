2d2b3189c5fc69c078c761d87424df0b491cad83
samiyonas <samuel.x.yonas@gmail.com>
2024-05-03 23:22:57 +0300

    first date

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" first state """
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(argv[1], argv[2], argv[3])
+            )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    que = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(que.id, que.name))
