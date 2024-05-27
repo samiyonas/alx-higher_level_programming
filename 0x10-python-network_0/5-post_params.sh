e3d4d72c086cac7be553d289260d5e927ec235de
samiyonas <samuel.x.yonas@gmail.com>
2024-05-27 07:39:42 -0700

    curl post parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # curl post parameters
-email="test@gmail.com"
-subject="I will always be here for PLD"
-curl -sL -X "POST" -d "email=$email&subject=$subject" $1
+curl -sL -X "POST" -d "email=test@gmail.com&subject=I will always be here for PLD" $1
