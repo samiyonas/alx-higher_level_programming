ad3440c24199a88ac156c72fcfcab956d76aab9b
samiyonas <samuel.x.yonas@gmail.com>
2024-05-28 09:14:43 -0700

    response header value 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@
 import urllib.request
 import sys
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get("X-Request-Id"))
