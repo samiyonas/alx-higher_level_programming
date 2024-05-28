7be64593c3282aa784ac837a61192794d19776d3
samiyonas <samuel.x.yonas@gmail.com>
2024-05-28 14:05:45 -0700

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" search API """
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        alpha = sys.argv[1]
+    else:
+        alpha = ""
+    info = {"q": alpha}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=info)
+    try:
+        js = response.json()
+        if not js:
+            print("No result")
+        else:
+            print("[<{}>] <{}>".format(js.get("id"), js.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
