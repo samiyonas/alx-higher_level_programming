a3c07e66ce7d1d0a739dd42eae32500b83b6150a
samiyonas <samuel.x.yonas@gmail.com>
2024-05-28 10:06:40 -0700

    what is my status 1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" what my status 1 """
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
