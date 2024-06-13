3552f39ef1ddc27857702779dec821082ea7d002
samiyonas <samuel.x.yonas@gmail.com>
2024-06-13 23:09:40 +0300

    second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let arr = process.argv.slice(2);
-  arr = arr.sort();
+  arr = arr.sort((a, b) => a - b);
+  console.log(arr)
   console.log(arr[arr.length - 2]);
 }
