7ba208bd5a87e370ffc2d03ee8e813e1c4b80c6f
samiyonas <samuel.x.yonas@gmail.com>
2024-06-13 23:01:28 +0300

    second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-  const list = process.argv.slice(2, process.argv.length - 1);
-  let max = 
+  let arr = process.argv.slice(2);
+  arr = arr.sort();
+  console.log(arr[arr.length - 2]);
+}
