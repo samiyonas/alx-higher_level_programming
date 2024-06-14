22aeb5c442b8bb0aa76e3490e177e228ec347a7e
samiyonas <samuel.x.yonas@gmail.com>
2024-06-15 02:48:21 +0300

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  width;
+  height;
+  constructor (w, h) {
+    if (w > 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
