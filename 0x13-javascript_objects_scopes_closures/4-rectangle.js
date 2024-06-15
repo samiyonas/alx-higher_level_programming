3b81a158270de49fb0b3fc3c2bf37ba3cbad0f06
samiyonas <samuel.x.yonas@gmail.com>
2024-06-15 03:05:26 +0300

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
