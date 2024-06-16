d15c3c42624daa4bb904c92832f0c95e5a3d45b2
samiyonas <samuel.x.yonas@gmail.com>
2024-06-16 18:34:19 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const fs = require("fs");
-fs.readFile(process.argv[2], "utf-8", (error, body) => {
-    if (error){
-        console.log(error);
-    }
-    else {
-        console.log(body.toString());
-    }
-})
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (error, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(body.toString());
+  }
+});
