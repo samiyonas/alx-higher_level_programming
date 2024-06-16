f2b8b6a960c9436377e469eb83cf46295a28eb54
samiyonas <samuel.x.yonas@gmail.com>
2024-06-16 18:57:25 +0300

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Code: ${response.statusCode}`);
+  }
+});
