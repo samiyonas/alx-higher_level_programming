67b19cf424a93369ba2ac685c8f3a93e54f24c18
samiyonas <samuel.x.yonas@gmail.com>
2024-06-16 23:47:39 +0300

    right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,28 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const cont = JSON.parse(body);
-  for (let i = 0; i < cont.characters.length; i++) {
-    request.get(cont.characters[i], (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const anoCont = JSON.parse(body);
-      console.log(anoCont.name);
-    });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  } else {
+    console.error(error);
   }
 });
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    } else {
+      console.error(error);
+    }
+  });
+}
