daee4d87ef198ba758125dcbc22dda3d68581c1b
samiyonas <samuel.x.yonas@gmail.com>
2024-06-16 20:36:44 +0300

    star wars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
+url = 'https://swapi-api.alx-tools.com/api/people/18/';
 request.get(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
@@ -8,10 +9,10 @@ request.get(process.argv[2], (error, response, body) => {
   }
   const cont = JSON.parse(body);
   let counter = 0;
-  for (let i = 0; i < cont.results.length; i++) {
-    if (cont.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+  cont.results.forEach((films) => {
+    if (films.characters.includes(url)){
       counter++;
     }
-  }
+  })
   console.log(counter);
 });
