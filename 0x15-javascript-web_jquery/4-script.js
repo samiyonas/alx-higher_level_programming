4aed91e822d17fb6afd79ea1784a23f1ead2bb21
samiyonas <samuel.x.yonas@gmail.com>
2024-06-23 17:25:31 +0300

    toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+$('#toggle_header').click(function () {
+  if ($('header').hasClass('red')) {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  } else if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  }
+});
