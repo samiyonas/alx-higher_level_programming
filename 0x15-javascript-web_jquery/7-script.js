73620b69d10f29b4a5ad72de9d05f772764d160d
samiyonas <samuel.x.yonas@gmail.com>
2024-06-23 17:50:44 +0300

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+$.ajax({
+    type: "GET",
+    url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+    success: function (data) {
+        $("#character").text(data["name"])
+    }
+})
